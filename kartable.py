24f7d35d6120e9b274a068297d8114ca7359340a
t0pl <37243285+t0pl@users.noreply.github.com>
2020-03-20 17:15:12 +0100

    Added upcoming features

--- kartable.py
+++ kartable.py
@@ -1,22 +1,252 @@
+"""
+Reuires a webbrowser and the helium module
+The hashlib module isn't needed basically as I had to find an easy way to get different a password for each account
+BUT, kartable.fr accepts pretty much every fuckin' email, name, password and birthdate without complaining
+so you could create an account with a non-existing email
+
+Most of the features are unstable and I'll certainly add a log file to handle errors
+"""
 import helium
 import hashlib
+import time
+from selenium.webdriver.support.ui import Select
 
-driver = helium.start_chrome("https://temp-mail.org/en/",headless=True)
-helium.wait_until(lambda: "@" in helium.TextField(
-    "Your Temporary email address").value)
-email = helium.TextField("Your Temporary email address").value
-password = hashlib.sha1(email.encode()).hexdigest()
-print(email,password)
-helium.kill_browser()
-driver = helium.start_chrome("https://www.kartable.fr/inscription")
-helium.click("élève")
-helium.click("1ère")
-helium.click("s'inscrire avec un e-mail")
-helium.write("Cld",into="prénom")
-helium.write("Lokidod",into="nom")
-helium.write(email,into="adresse e-mail")
-helium.write(password,into="mot de passe")
-helium.click("terminer")
-helium.wait_until(helium.Text("plus tard").exists)
-helium.click("plus tard")
-#helium.kill_browser()
+class main:
+    def __init__(self, classe, firstname, lastname, email, password, birthdate=None, ecole="victor hugo", url="https://www.kartable.fr/inscription"):
+        # birthdate as follows : DD/MM/YYYY
+        self.url = url
+        self.classe = classe
+        self.firstname = firstname
+        self.lastname = lastname
+        self.email = email
+        self.password = password
+        self.ecole = ecole
+        self.birthdate = birthdate.split("/") if len(self.birthdate.split("/")) == 3 else None
+        self.driver = helium.start_chrome(self.url)
+
+    def register(self, sponso=False):
+        if sponso == False:
+            try:
+                helium.click("élève")
+                helium.click("élève")
+            except LookupError:
+                print("élève failed")
+        helium.click(self.classe)
+        helium.click("s'inscrire avec un e-mail")
+        helium.write(self.firstname, into="prénom")
+        helium.write(self.lastname, into="nom")
+        helium.write(self.email, into="adresse e-mail")
+        helium.write(self.password, into="mot de passe")
+        # Setting custom birthdate
+        self.set_birthdate()
+        helium.click("terminer")
+        # time.sleep(5)
+        try:
+            helium.click("plus tard")
+        except LookupError:
+            pass
+        time.sleep(5)
+
+    def log_out(self):
+        #!!!!!NOT TESTED YET!!!!!!
+        self.driver.get("https://www.kartable.fr/compte")
+        helium.click("Se déconnecter")
+        helium.click("SE DÉCONNECTER")
+
+    def log_in(self):
+        self.driver.get("https://www.kartable.fr/connexion")
+        helium.write(self.email, into="adresse e-mail")
+        helium.write(self.password, into="mot de passe")
+        helium.click("se connecter")
+
+    def join_school(self):
+        #!!!!!NOT TESTED YET!!!!!!
+        self.driver.get(
+            "https://www.kartable.fr/classe?navigationOrigin=ruby-quest")
+        helium.write(self.ecole, into="Rechercher un établissement...")
+        select = Select(self.driver.find_element_by_tag_name('select'))
+        select.select_by_value('1')  # select the first
+        helium.click("rejoindre")
+        self.driver.find_element_by_class_name("icon-close").click()
+
+    def delete_school(self):
+        return "Not implemented yet"
+
+    def change_email(self, new_email):
+        #!!!!!NOT TESTED YET!!!!!!
+        # https://www.kartable.fr/compte/informations-personnelles/modifier-adresse-email
+        for i in self.driver.find_element_by_tag_name("input"):
+            helium.write(new_email, into=i)
+        helium.click("mettre à jour")
+
+    def change_password(self, new_password):
+        #!!!!!NOT TESTED YET!!!!!!
+        # https://www.kartable.fr/compte/informations-personnelles/modifier-mot-de-passe
+        helium.write(self.password, into="Mot de passe actuel")
+        helium.write(new_password, into="Nouveau mot de passe")
+        helium.write(new_password, into="Ressaisir nouveau mot de passe")
+        helium.click("mettre à jour")
+
+    def is_locked(self, ressource):
+        return "Not implemented yet"
+        # self.driver.find_element_by_class_name("document__state")#if child has class icon-document-locked :True
+
+    @property
+    def current_email(self):
+        #!!!!!NOT TESTED YET!!!!!!
+        # https://www.kartable.fr/compte/informations-personnelles/modifier-adresse-email
+        return self.driver.find_element_by_tag_name("strong").text
+
+    def delete_account(self):
+        #!!!!!NOT TESTED YET!!!!!!
+        # https://www.kartable.fr/compte/informations-personnelles
+        helium.click("SUPPRIMER LE COMPTE")
+        helium.click("SUPPRIMER DÉFINITIVEMENT")
+
+    def levels_alternative(self):
+        return "Not implemented yet"
+        # requires status: not signed in
+        # requires url: https://www.kartable.fr/
+        tmp = self.driver.find_element_by_class_name("et_pb_text_inner")
+        for i in tmp:
+            if "Classes" == tmp.find_element_by_class_name("links-list__title"):
+                # tmp.find_element_by_tag_name("a").href +.text
+                pass
+
+    @property
+    def account_info(self):
+        #!!!!!NOT TESTED YET!!!!!!
+        # https://www.kartable.fr/compte/informations-personnelles
+        firstname = self.driver.find_element_by_xpath(
+            "//input[@name='firstName']")
+        lastname = self.driver.find_element_by_xpath(
+            "//input[@name='lastName']")
+        birthdate = ""
+        input__group = self.driver.find_element_by_class_name(
+            "input__group--date")  # includes a bunch of select tags
+        select = input__group.find_elements_by_tag_name('select')
+        for i in select:
+            birthdate += Select(i).first_selected_option.text+"/"
+        tmp = ""
+        for i in birthdate[:-1].split("/"):
+            tmp += "0"+i+"/" if len(i) == 1 else i + "/"
+        birthdate = tmp[:-1]
+        return {"firstname": firstname, "lastname": lastname, "birthdate": birthdate}
+
+    def set_birthdate(self):
+        #!!!!!NOT TESTED YET!!!!!!
+        if self.birthdate != None:
+            day_to_be_set = self.birthdate[0]
+            month_to_be_set = self.birthdate[1]
+            year_to_be_set = self.birthdate[2]
+            try:
+                input__group = self.driver.find_element_by_class_name(
+                    "input__group--date")  # includes a bunch of select tags
+                select = input__group.find_elements_by_tag_name('select')
+                for item in select:
+                    if item.get_attribute("name") == "birthDateDay":
+                        day = Select(item)
+                        day.select_by_value(day_to_be_set)
+                    elif item.get_attribute("name") == "birthDateMonth":
+                        month = Select(item)
+                        month.select_by_value(month_to_be_set)
+                    elif item.get_attribute("name") == "birthDateYear":
+                        year = Select(item)
+                        year.select_by_value(year_to_be_set)
+            except Exception as e:
+                print("Failed to set birthdate", e)
+
+    def subject_categories(self):
+        return "Not implemented yet"
+        #mm = [i for i in self.driver.find_elements_by_class_name("category__link")]
+
+    def delete_premium_ads(self):
+        #!!!!!NOT TESTED YET!!!!!!
+        self.driver.execute_script(
+            'try{document.getElementsByTagName("premium-link")[0].remove();}catch(error){console.error(error);}')
+        self.driver.execute_script(
+            'try{document.getElementsByClassName("cross-selling-ad")[0].parentElement.remove();}catch(error){console.error(error);}')
+
+    @property
+    def has_school(self):
+        return "Not implemented yet"
+
+    @property
+    def subjects(self, classe):
+        #!!!!!NOT TESTED YET!!!!!!
+        self.unavailable = []
+        self.courses_info = []
+        # let it redirect to homepage to avoid spelling mistakes (instead of get("https://www.kartable.fr/cinquieme") for eg)
+        self.driver.get("https://www.kartable.fr/")
+        try:
+            self.courses_info = [{i.text: i.get_attribute("href")} for i in self.driver.find_elements_by_class_name(
+                "course__link")]  # href is None if not available
+        except Exception as e:
+            print("Failed to get course titles", e)
+            self.courses_info = False
+
+        try:
+            for i in self.driver.find_elements_by_class_name("course--coming-soon"):
+                self.unavailable.append(
+                    i.find_element_by_class_name("course__link").text)
+        except Exception as e:
+            print("Failed to get name of future available subjects")
+            self.unavailable = False
+        return self.courses_info+self.unavailable
+
+    @property
+    def levels(self):
+        return "Not implemented yet"
+        """https://www.kartable.fr/{classe}"""
+        helium.click(self.current_level)
+        m = list(set(
+            [i.text for i in self.driver.find_elements_by_class_name("school-year__level")]))
+        self.driver.find_element_by_class_name("icon-close").click()
+
+    @property
+    def current_level(self):
+        try:
+            level = self.driver.find_element_by_class_name(
+                "school-year__level").text
+        except Exception as e:
+            print("Failed to get the level", e)
+            level = False
+        finally:
+            return level
+
+    @property
+    def sponso_url(self):
+        self.driver.get(
+            "https://www.kartable.fr/compte/parrainage?questIdentifier=sponsorship_fifth")
+        time.sleep(5)
+        self.driver.execute_script(
+            'try{document.getElementsByTagName("input")[0].removeAttribute("readonly");}catch(error){console.error(error);}')
+        self._sponso_url = self.driver.execute_script(
+            'return document.getElementsByTagName("input")[0].value;')
+        if not self._sponso_url.startswith("https://"):
+            return False
+        return self._sponso_url
+
+    @property
+    def ruby_amount(self):
+        """Works for https://www.kartable.fr/rubis, https://www.kartable.fr/cinquieme, https://www.kartable.fr/ (because redirects to .../{classe}"""
+        try:
+            rubies = self.driver.find_element_by_class_name(
+                "ruby-counter__total").text
+        except Exception as e:
+            print("Failed to get the amount of rubies", e)
+            rubies = False
+        finally:
+            return rubies
+
+
+"""
+Usage:
+    instance = main("6e","philotea","rotrions","manlymprinc1241478451ent@gmail.com","xfcgvhibu1457hoomskpjoihugyf")
+    time.sleep(5)
+    instance.register()
+    ...
+    url = instance.sponso_url
+    instance.log_out()
+
+"""
